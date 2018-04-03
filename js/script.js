b5bcbdfd878bd6e698ae893b93560ae5a0cabd86
Dj0ko <antonr1@bk.ru>
2018-04-03 13:50:36 +0300

    Исправил замечания и добавил js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
 "use strict";
+
 var btnWindowChangeType = document.querySelector(".button-search");
 
-var form = document.querySelector(".searching-form");
+var popup = document.querySelector(".modal");
 
-var arrival = form.querySelector("[name=arrival-date]");
+var form = popup.querySelector("form");
+
+var arrival = popup.querySelector("[name=arrival-date]");
+
+var departure = popup.querySelector("[name=date-of-departure]");
+
+var amountOfAdults = popup.querySelector("[name=amount-of-adults]");
+
+var amountOfChildren = popup.querySelector("[name=amount-of-children]")
 
 btnWindowChangeType.addEventListener("click", function (evt) {
-  evt.preventDefault();  
-  form.classList.toggle("searching-form-change");
-  arrival.focus();
-});                                 
+    evt.preventDefault();  
+    popup.classList.toggle("modal-change");
+    arrival.focus();
+}); 
+
+form.addEventListener("submit", function (evt) {
+    if (!arrival.value || !departure.value || !amountOfAdults.value || !amountOfChildren.value) {
+        evt.preventDefault();
+        console.log("Необходимо заполнить все поля");
+    }   
+});   
